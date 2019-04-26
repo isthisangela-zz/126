a19300c59cb6d1e09f64eb746cf253eaf5f44513
Angela Dong <angela.dong@berkeley.edu>
2019-04-26 01:13:00 -0700

    ready to go

--- vis.py
+++ vis.py
@@ -198,7 +198,7 @@ class GraphFrame(wx.Frame):
         self.fig = Figure((3.0, 3.0), dpi=self.dpi)
 
         self.axes = self.fig.add_subplot(111)
-        self.axes.set_axis_bgcolor('black')
+        self.axes.set_facecolor('black')
         self.axes.set_title('Very important random data', size=12)
 
         pylab.setp(self.axes.get_xticklabels(), fontsize=8)
@@ -327,7 +327,7 @@ class GraphFrame(wx.Frame):
 
 
 if __name__ == '__main__':
-    app = wx.PySimpleApp()
+    app = wx.App()
     app.frame = GraphFrame()
     app.frame.Show()
     app.MainLoop()
